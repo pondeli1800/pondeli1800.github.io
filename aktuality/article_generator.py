3de0ea9bf8a2be723ad514708e2ae29d42c3cf86
pondeli18.00 <pondeli18.00@gmail.com>
2025-12-22 20:12:31 +0100

    fix typo

--- aktuality/article_generator.py
+++ aktuality/article_generator.py
@@ -9,7 +9,7 @@ title = "Mandava Jazz 2026"
 description = "V říjnu jsem si zahráli na Mandava Jazzu a bylo to mega super"
 youtube_link = ""  # Leave empty if no video
 video_title = "Edith with the Stick #livemusic #concert"
-url_slug = "Aandava-jazz-2026"
+url_slug = "mandava-jazz-2026"
 output_file = f"aktuality/{url_slug}/index.html"
 
 # =========================
